9c1234a68a06a3430f7dd41de843af31fe7ccde2
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-02-14 15:39:06 +0700

    (user s.) user get API

--- service-user/routes/handler/users/index.js
+++ service-user/routes/handler/users/index.js
@@ -1,10 +1,12 @@
 const register = require('./register')
 const login = require('./login')
 const update = require('./update')
+const getUser = require('./getUser')
 
 
 module.exports = {
     register,
     login,
-    update
+    update,
+    getUser
 }
--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const userHandler = require('./handler/users')
 
+/* GET users listing. */
+router.get('/:id', userHandler.getUser)
+
 /* POST users listing. */
 router.post('/register', userHandler.register);
 router.post('/login', userHandler.login);
