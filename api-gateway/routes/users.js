94397b0af4572be864e4b914b7c0f6f1b28e94ec
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-02-15 14:14:58 +0700

    (user s.) LOGIN API Integration

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -5,5 +5,6 @@ const userHandler = require('./handler/users')
 
 /* POST users listing. */
 router.post('/register', userHandler.register);
+router.post('/login', userHandler.login);
 
 module.exports = router;
