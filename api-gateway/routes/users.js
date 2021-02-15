875322b2aa118b59fdce65cdf4c616f9a857b3a8
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-02-15 15:18:55 +0700

    (user s.) GET User intgration

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -4,11 +4,14 @@ var router = express.Router();
 const userHandler = require('./handler/users')
 const verifyToken = require('./../middlewares/verifyToken')
 
-/* PUT users listing. */
-router.put('/', verifyToken, userHandler.update);
-
 /* POST users listing. */
 router.post('/register', userHandler.register);
 router.post('/login', userHandler.login);
+
+/* PUT users listing. */
+router.put('/', verifyToken, userHandler.update);
+
+/* GET users listing. */
+router.get('/', verifyToken, userHandler.getUser);
 
 module.exports = router;
