11585f77f10038ea56577c6df54bb8fd3f3eb3bf
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-02-16 07:56:07 +0700

    (course s.) ImageCourse & Mentor Model

---
+++ service-course/app/Mentor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mentor extends Model
+{
+    //
+    protected $table = 'mentors';
+
+    protected $fillable = [
+        'name', 'profile', 'email', 'profession'
+    ];
+}
