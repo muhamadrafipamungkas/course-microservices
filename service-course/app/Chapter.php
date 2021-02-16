be87dac42766e78cb675f8b05860f4a7f0678195
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-02-16 07:36:29 +0700

    (course s.) Chapter & Lesson Model

---
+++ service-course/app/Chapter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Chapter extends Model
+{
+    //
+    protected $table = 'chapters';
+
+    protected $fillable = [
+        'name', 'course_id'
+    ];
+
+    /**
+     * Get all of the lesson for the Chapter
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function lesson(): HasMany
+    {
+        return $this->hasMany(Lesson::class)->orderBy('id', 'ASC');
+    }
+}
