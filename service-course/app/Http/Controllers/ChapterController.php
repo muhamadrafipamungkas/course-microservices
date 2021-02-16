935677432b3c8adb423454aa9bcfb8f418878812
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-02-16 17:46:50 +0700

    (course) create chapter api

---
+++ service-course/app/Http/Controllers/ChapterController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Validator;
+
+use App\Chapter;
+use App\Course;
+
+class ChapterController extends Controller
+{
+    //
+    public function create(Request $request) {
+        $rules = [
+            'name' => 'required|string',
+            'course_id' => 'required|integer'
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $courseId = $request->input('course_id');
+        $course = Course::find($courseId);
+
+        if (!$course) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'course not found'
+            ], 404);
+        }
+
+        $chapter = Chapter::create($data);
+        return response()->json([
+            'status' => 'success',
+            'data' => $chapter
+        ], 200);
+    }
+}
