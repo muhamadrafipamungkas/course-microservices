a418d31751f177b36db2e128a54372579b5c3339
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-02-18 19:15:57 +0700

    (course) chapters create, update, delete integrati

---
+++ api-gateway/routes/chapters.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const {APP_NAME} = process.env;
+
+const chaptersHandler = require('./handler/chapters')
+
+const verifyToken = require('./../middlewares/verifyToken')
+
+/* GET media listing. */
+// router.get('/', chaptersHandler.getAll)
+// router.get('/:id', chaptersHandler.get)
+
+/* POST media listing. */
+router.post('/', chaptersHandler.create)
+
+/* PUT media listing. */
+router.put('/:id', chaptersHandler.update)
+
+/* DELETE media listing. */
+router.delete('/:id', chaptersHandler.destroy)
+
+module.exports = router;
