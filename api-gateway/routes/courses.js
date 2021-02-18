f8888034f7f55b401bc4648fba4a0edabfcf8fac
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-02-18 18:18:30 +0700

    (course) course create, delete, update integration

--- api-gateway/routes/courses.js
+++ api-gateway/routes/courses.js
@@ -2,9 +2,19 @@ var express = require('express');
 var router = express.Router();
 const {APP_NAME} = process.env;
 
-/* GET courses listing. */
-router.get('/', function(req, res, next) {
-  res.send('courses');
-});
+const coursesHandler = require('./handler/courses')
+
+/* GET media listing. */
+// router.get('/', coursesHandler.getAll)
+// router.get('/:id', coursesHandler.get)
+
+/* POST media listing. */
+router.post('/', coursesHandler.create)
+
+/* PUT media listing. */
+router.put('/:id', coursesHandler.update)
+
+/* DELETE media listing. */
+router.delete('/:id', coursesHandler.destroy)
 
 module.exports = router;
