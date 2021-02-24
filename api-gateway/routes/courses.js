f9cbfb873b89f4b9d1279613565aed36efe2c5a5
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-02-24 14:44:02 +0700

    role and permission middleware [GATEWAY]

--- api-gateway/routes/courses.js
+++ api-gateway/routes/courses.js
@@ -5,18 +5,19 @@ const {APP_NAME} = process.env;
 const coursesHandler = require('./handler/courses')
 
 const verifyToken = require('./../middlewares/verifyToken')
+const can = require('./../middlewares/permission')
 
 /* GET media listing. */
 router.get('/', coursesHandler.getAll)
 router.get('/:id', coursesHandler.get)
 
 /* POST media listing. */
-router.post('/', verifyToken, coursesHandler.create)
+router.post('/', verifyToken, can('admin'), coursesHandler.create)
 
 /* PUT media listing. */
-router.put('/:id', verifyToken, coursesHandler.update)
+router.put('/:id', verifyToken, can('admin'), coursesHandler.update)
 
 /* DELETE media listing. */
-router.delete('/:id', verifyToken, coursesHandler.destroy)
+router.delete('/:id', verifyToken, can('admin'), coursesHandler.destroy)
 
 module.exports = router;
